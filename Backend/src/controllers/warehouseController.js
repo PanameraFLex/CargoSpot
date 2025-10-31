bdf393cd86e81b3a6c29bd2be110e97a461e8a2c
ChuxInCode <chuxmonye@gmail.com>
2025-10-31 10:00:21 +0000

    full crud capability

--- Backend/src/controllers/warehouseController.js
+++ Backend/src/controllers/warehouseController.js
@@ -28,3 +28,42 @@ export const createWarehouse = async(req,res)=>{
 		res.status(400).json({message:"Invalid data"})
 	}
 };
+// update existing warehouse
+export const updateWarehouseById = async(req,res)=>{
+	try{
+		const updated = await Warehouse.findByIdAndUpdate(req.params.id, req.body,{new:true});
+	if(!updated) return res.status(404).json({message:"Warehouse not found"});	
+	res.json(updated)
+	}catch(error){
+		res.status(500).json({message:"Update failed"})
+	}
+};
+
+export const deleteWarehouseById = async(req,res)=>{
+	try{
+		const deleted = await Warehouse.findByIdAndDelete(req.params.id);
+		if(!deleted) return res.status(404).json({message:"Warehouse not found"});
+		res.status(200).json({message:"Warehouse deleted successfully"})
+	}catch(err){
+		res.status(500).json({message:"Unable to delete"})
+	}
+};
+
+export const searchWarehouseById = async(req,res)=>{
+	const { name, manager, minCap, maxCap } = req.query;
+	const query = {};
+	if (name) query.name = {$regex: name, $options: "i"};
+	if (manager) query.manager = {$regex: manager, $options: "i"};
+	if (mincap || maxCap)
+		query.capacity = {
+		...(minCap && { $gte: parseInt(minCap)}),
+		...(maxCap && { $lte: parseInt(maxCap)}),
+	};
+	try{
+		const results = await Warehouse.find(query);
+		res.json(results);
+	} catch(err){
+		console.error(err);
+		res.status(500).json({ message: "Search error" });
+	}
+}
--- Backend/src/routes/warehouseRoutes.js
+++ Backend/src/routes/warehouseRoutes.js
@@ -1,9 +1,34 @@
 import express from "express";
-import { getWarehouses,getWarehouseById,createWarehouse } from "../controllers/warehouseController.js";
+import { getWarehouses,getWarehouseById,createWarehouse,updateWarehouseById, deleteWarehouseById, searchWarehouseById} from "../controllers/warehouseController.js";
 const router = express.Router();
 
 router.route("/").get(getWarehouses).post(createWarehouse);
-router.route("/:id").get(getWarehouseById);
+router.route("/:id").get(getWarehouseById).put(updateWarehouseById);
+router.route("/:id").get(getWarehouseById).delete(deleteWarehouseById);
+router.route("/search").get(searchWarehouseById);
+//update existing warehouse
+// router.put("/:id", async (req, res)=>{
+// 	try{
+// 		const updated = await Warehouse.findByIdAndUpdate(req.params.id, params.body,{new:true});
+// 	if(!updated) return res.status(404).json({message:"Warehouse not found"})
+// 	res.json(updated)
+// 	}catch(err){
+// 		console.error(err)
+// 		res.status(500).json({message:"Update failed"})
+// 	}
+// })
+
+//delete existing warehouse by ID
+router.delete("/:id", async(req, res)=>{
+	try{
+		const deleted = await Warehouse.findByIdAndDelete(req.params.id);
+		if(!deleted)return
+			res.status(404).json({message:"Warehouse not found"})
+		res.json({message:"Warehouse deleted"})
+		}catch(err){
+		res.status(500).json({message:"Unable to delete"})
+	}
+})
 
 // router.get("/", (req,res)=>{
 // 	res.json({message: "Warehouse API is working 🚀"})
